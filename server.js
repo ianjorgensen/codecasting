22300e69acc14bd8861568abd1f5407e9c1cffbc
Ian Jorgensen <jorgensen.ian@gmail.com>
2011-12-12 23:33:08 +0100

    changed audio src

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var server = require('router').create();
 var bark = require('bark');
 
-var port = parseInt(process.argv[2] || 8080, 10);
+var port = process.argv[2] || 8080;
 
 server.get('/', bark.file('./index.html'));
 server.get('/css/*', bark.file('./css/{*}'));
